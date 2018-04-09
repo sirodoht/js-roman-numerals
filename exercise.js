173162eaef0dcff31e9f947f25fc58b1a38774a4
Theodore Keloglou <theodorekeloglou@gmail.com>
2018-04-09 16:50:44 +0300

    cleanup comments

--- exercise.js
+++ exercise.js
@@ -19,9 +19,7 @@ var ROMAN_TO_DIGIT = {
 function RomanNumber(num) {
   if (Number.isInteger(num)) {
     this.number = num;
-    // this.roman = RomanNumber.toString(num);
   } else if (RomanNumber.isValid(num)) {
-    // this.number = RomanNumber.toInt(num);
     this.roman = num;
   } else {
     throw new Error('invalid value');
@@ -29,7 +27,6 @@ function RomanNumber(num) {
 }
 
 RomanNumber.prototype.toInt = function () {
-  console.log('toInt:', this.roman);
   var number = 0;
   var doubleLetter = false;
   for (var i = 0; i < this.roman.length; i++) {
@@ -38,27 +35,19 @@ RomanNumber.prototype.toInt = function () {
     if (i >= this.roman.length - 1) {
       nextLetter = '';
     }
-    console.log('currentLetter:', currentLetter);
-    console.log('nextLetter:', nextLetter);
-    console.log('ROMAN_TO_DIGIT[nextLetter]:', ROMAN_TO_DIGIT[nextLetter]);
-    console.log('ROMAN_TO_DIGIT[currentLetter]:', ROMAN_TO_DIGIT[currentLetter]);
     if (!doubleLetter && nextLetter && ROMAN_TO_DIGIT[nextLetter] <= ROMAN_TO_DIGIT[currentLetter]) {
       number += ROMAN_TO_DIGIT[currentLetter];
     }
     if (doubleLetter) {
       var previousLetter = this.roman.charAt(i - 1);
-      console.log('ROMAN_TO_DIGIT[currentLetter] - ROMAN_TO_DIGIT[previousLetter]:', ROMAN_TO_DIGIT[currentLetter] - ROMAN_TO_DIGIT[previousLetter]);
       number += (ROMAN_TO_DIGIT[currentLetter] - ROMAN_TO_DIGIT[previousLetter]);
       doubleLetter = false;
     } else if (!nextLetter) {
-      console.log('adding:', ROMAN_TO_DIGIT[currentLetter]);
       number += ROMAN_TO_DIGIT[currentLetter];
     }
     if (nextLetter && ROMAN_TO_DIGIT[nextLetter] > ROMAN_TO_DIGIT[currentLetter]) {
       doubleLetter = true;
     }
-    console.log('sum now:', number);
-    console.log('');
   }
   return number;
 }
@@ -77,47 +66,31 @@ RomanNumber.prototype.toString = function () {
     1: 'I',
   };
 
-  console.log('this.number:', this.number);
-
   var modRatio = 10000;
   var divideRatio = 1000;
   var letter = 'M';
   while (!done) {
-    console.log('modRatio:', modRatio);
-    console.log('divideRatio:', divideRatio);
-    console.log('letter:', letter);
 
     var scale = Math.floor((this.number % modRatio) / divideRatio);
-    console.log('scale:', scale);
     if (scale <= 3) {
       for (var i = 0; i < scale; i++) {
         roman += letter;
       }
     } else if (scale === 4) {
-      console.log('scale 4 calc');
       var scaleMultiplied = scale * divideRatio;
-      console.log('scaleMultiplied:', scaleMultiplied);
-      console.log(':RomanNumber.getRoman(scaleMultiplied):', RomanNumber.getRoman(scaleMultiplied));
       roman += RomanNumber.getRoman(scaleMultiplied);
 
     } else if (scale >= 5 && scale < 9) {
-      console.log('scale 5, 6, 7, 8 calc');
       var scaleMultiplied = scale * divideRatio;
       var halfScale = modRatio / 2;
       roman += RomanNumber.getRoman(halfScale);
-      console.log('scaleMultiplied:', scaleMultiplied);
-      console.log('halfScale:', halfScale);
-      console.log('roman now (half):', roman);
 
       var copiesLeft = scale - 5;
       for (var i = 0; i < copiesLeft; i++) {
         roman += letter;
       }
-      console.log('roman now:', roman);
     } else if (scale === 9) {
-      console.log('scale 9 calc');
       var scaleMultiplied = scale * divideRatio;
-      console.log(':RomanNumber.getRoman(scaleMultiplied):', RomanNumber.getRoman(scaleMultiplied));
       roman += RomanNumber.getRoman(scaleMultiplied);
     }
 
@@ -127,10 +100,8 @@ RomanNumber.prototype.toString = function () {
     modRatio /= 10;
     divideRatio /= 10;
     letter = letterDict[divideRatio];
-    console.log('');
   }
 
-  console.log('final roman:', roman);
   return roman;
 }
 
@@ -157,25 +128,36 @@ testRoman = function () {
   // console.log(RomanNumber.isValid('C'));
   // console.log(RomanNumber.isValid('X'));
 
-  // var romanNumbers = {
-  //   'XI': 11,
-  //   'CDXXIX': 429,
-  //   'MCDLXXXII': 1482,
-  //   'MCMLXXX': 1980,
-  //   'MMMMCMXCIX': 4999,
-  //   'MMMMDMXCIX': 4599,
-  // };
-  // Object.keys(romanNumbers).forEach(function (item) {
-  //   var number = new RomanNumber(item);
-  //   if (number.toInt() !== romanNumbers[item]) {
-  //     throw new Error('.toInt() failed');
-  //   }
-  // });
+  var romanNumbers = {
+    'XI': 11,
+    'CDXXIX': 429,
+    'MCDLXXXII': 1482,
+    'MCMLXXX': 1980,
+    'MMMMCMXCIX': 4999,
+    'MMMMDMXCIX': 4599,
+  };
+  Object.keys(romanNumbers).forEach(function (item) {
+    var number = new RomanNumber(item);
+    if (number.toInt() !== romanNumbers[item]) {
+      throw new Error('.toInt() failed');
+    }
+  });
 
-  // console.log('getRoman:', RomanNumber.getRoman(900));
+  var romanNumbersInverse = {
+    '1945': 'MCMXLV',
+    '1968': 'MCMLXVIII',
+    '1473': 'MCDLXXIII',
+    '2999': 'MMCMXCIX',
+    '3000': 'MMM',
+    '3999': 'MMMCMXCIX',
+  };
+  Object.keys(romanNumbersInverse).forEach(function (item) {
+    var number = new RomanNumber(parseInt(item));
+    if (number.toString() !== romanNumbersInverse[item]) {
+      throw new Error('.toString() failed');
+    }
+  });
 
-  var romanNumber1 = new RomanNumber(1945);
-  console.log('1968 toString:', romanNumber1.toString());
 };
 
 testRoman();
