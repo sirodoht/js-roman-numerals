7ee13f6f9e5d1657a5b0b5980e4094e6804685ca
Theodore Keloglou <theodorekeloglou@gmail.com>
2018-04-09 15:19:13 +0300

    commit progress

--- exercise.js
+++ exercise.js
@@ -9,6 +9,7 @@ var ROMAN_TO_DIGIT = {
   'D': 500,
   'M': 1000,
 };
+// MCMLIII
 
 function RomanNumber(num) {
   if (Number.isInteger(num)) {
@@ -23,7 +24,19 @@ function RomanNumber(num) {
 }
 
 RomanNumber.prototype.toInt = function () {
-  this.roman
+  var number = 0;
+  for (var i = 0; i < this.roman.length; i++) {
+    var currentLetter = this.roman.charAt(i);
+    var nextLetter = this.roman.charAt(i + 1);
+    if (i >= this.roman.length - 1) {
+      nextLetter = '';
+    }
+    console.log('currentLetter:', currentLetter);
+    console.log('nextLetter:', nextLetter);
+    console.log('ROMAN_TO_DIGIT[nextLetter]:', ROMAN_TO_DIGIT[nextLetter]);
+    console.log('ROMAN_TO_DIGIT[currentLetter]:', ROMAN_TO_DIGIT[currentLetter]);
+    if (nextLetter && ROMAN_TO_DIGIT[nextLetter] > ROMAN_TO_DIGIT[currentLetter]) {}
+  }
 }
 
 RomanNumber.isValid = function (numString) {
@@ -39,7 +52,8 @@ testRoman = function () {
   // console.log(RomanNumber.isValid('s'));
   // console.log(RomanNumber.isValid('C'));
   // console.log(RomanNumber.isValid('X'));
-  var romanNumber1 = new RomanNumber('XX');
+  var romanNumber1 = new RomanNumber('XI');
+  console.log('romanNumber1.toInt():', romanNumber1.toInt());
   // if (romanNumber1.toInt() !== 20) {
   //   throw new Error();
   // }
