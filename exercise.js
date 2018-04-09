db7208211d59ac4af66efa84b24f2eb589a6039b
Theodore Keloglou <theodorekeloglou@gmail.com>
2018-04-09 14:41:51 +0300

    update test case

--- exercise.js
+++ exercise.js
@@ -1,18 +1,11 @@
-var testChars = ['I', 'V', 'X', 'C'],
-
-  isItTen = function (num) {
-    return num.toLowerCase() === 'x';
-  },
-
-  isItFive = function (num) {
-    return num.toLowerCase() === 'v';
-  },
-
-  testRoman = function () {
-    testChars.forEach(function (str) {
-      console.log('Is %s 10? ', str, (isItTen(str) ? 'Yes' : 'No'));
-      console.log('Is %s 5? ', str, (isItFive(str) ? 'Yes' : 'No'));
-    });
-  };
+testRoman = function () {
+  var romanNumber1 = new RomanNumber('XX');
+  if (romanNumber1.toInt() !== 20) {
+    throw new Error();
+  }
+  if (romanNumber1.toString() !== 'XX') {
+    throw new Error();
+  }
+};
 
 testRoman();
